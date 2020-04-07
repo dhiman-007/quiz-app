a49fbadf25760a259536367cefcb8652e3a8f6c5
dhiman-007 <shubhamdhiman7862@gmail.com>
2020-04-07 22:04:10 +0530

    Commit

---
+++ ext.js
@@ -0,0 +1,141 @@
+
+
+var myQuestion=[
+	{
+    question: "In India, who heads the Department of Space?",
+    answers: {
+      a: "The president" ,
+	  b: "The prime minister" ,
+	  c: "The defence minister"
+    },
+    correctAnswer: "b"
+  },
+	 {
+    question: "Which type of file uses the .MOBI extension?",
+    answers: {
+      a: "e-book",
+      b: "Compressed Image",
+      c: "Audio file"
+    },
+    correctAnswer: "a"
+  },
+	{
+    question: "Recording data on a DVD is also called what?",
+    answers: {
+      a: "Enulating",
+      b: "Burning",
+      c: "Tracking"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "What year was the very first model of the iPhone released?",
+    answers: {
+      a: "2008",
+      b: "2009",
+      c: "2007"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which planet has the most gravity?",
+    answers: {
+      a: "Earth",
+      b: "Jupiter",
+      c: "venus"
+    },
+    correctAnswer: "b"
+	},
+	{
+    question: "How many hearts does an Octopus have?",
+    answers: {
+      a: "2",
+      b: "3",
+      c: "4"
+    },
+    correctAnswer: "b"
+	},
+	{
+    question: "Which is the Largest Country By Area?",
+    answers: {
+      a: "China",
+      b: "Russia",
+      c: "U.S.A"
+    },
+    correctAnswer: "b"
+	},
+	{
+    question: "Sir Winston Churchill visited which country <br> during its years under British rule  and called it 'the pearl of Africa'?",
+    answers: {
+      a: "Uganda",
+      b: "Kenya",
+      c: "Somalia"
+    },
+    correctAnswer: "a"
+	},
+	{
+    question: "In 1976, in which catgeory did Sholay win its only Filmfare Award?",
+    answers: {
+      a: "Best Director",
+      b: "Best Film",
+      c: "Best Editing"
+    },
+    correctAnswer: "c"
+	},
+	{
+    question: "Shortcut for Renaming is?",
+    answers: {
+      a: "f3",
+      b: "f2",
+      c: "f5"
+    },
+    correctAnswer: "b"
+	},
+	{
+    question: "What is the old name for Sri Lanka?",
+    answers: {
+      a: "Mandalay",
+      b: "Ceylon",
+      c: "East Tamilnadu"
+    },
+    correctAnswer: "b"
+	},
+	{
+    question: "Is electrolysis same to ionization? If no, why?",
+    answers: {
+      a: "Yes",
+      b: "Matter split up into ions",
+      c: "Electrolysis can't be done without ionization"
+    },
+    correctAnswer: "c"
+	},
+	{
+    question: "BASIC, C and COBOL and Java <br/> are examples of which language?",
+    answers: {
+      a: "Low level",
+      b: "High Level",
+      c: "System Programming"
+    },
+    correctAnswer: "b"
+	},
+	{
+    question: "The working principle of a washing machine is?",
+    answers: {
+      a: "Centrifugation",
+      b: "Reverse Osmosis",
+      c: "Dialysis"
+    },
+    correctAnswer: "a"
+	},
+	{
+    question: "Which of these classes is not <br/>part of Java collection framework?",
+    answers: {
+      a: "Array",
+      b: "Queue",
+      c: "Maps"
+    },
+    correctAnswer: "b"
+	}
+  
+  
+];
